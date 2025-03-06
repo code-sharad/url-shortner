aecd759a16411108af9f81055bba7354484b1f57
Nikhil Bhadait <codesharad@gmail.com>
2025-03-06 06:43:15 +0530

    fix the env

--- index.js
+++ index.js
@@ -82,5 +82,5 @@ app.delete("/link/:id", async (req, res) => {
 
 // Start the server
 app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${process.env.PORT}`);
 });
